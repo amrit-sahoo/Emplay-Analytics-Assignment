ffc91e70e3adf3c80deabf416736f0e97487ca00
Amrit Kumar Sahoo <31152533+amrit-sahoo@users.noreply.github.com>
2018-01-11 15:13:10 +0530

    Add files via upload

--- angular/controllers/search_controller.js
+++ angular/controllers/search_controller.js
@@ -3,23 +3,49 @@ mainApp.controller('repoSearch' , ['$http','gitService' , function($http , gitSe
 	var main = this;
 
 	this.heading = "Enter The Repository Name For Search";
-    // this.query = ;
-	//console.log(this.query)
-	this.getRepository = function(){
-        var q = main.query;
-        console.log(main.query);
-        gitService.search(q)
+    
+    
+    
+this.typingTimer; //timer identifier
+  
+this.array=[]; 
+
+
+
+
+
+
+    this.getRepository = function(){
+         main.Name= document.forms["searchbox"]["key"].value;
+         console.log(main.Name);
+        
+        gitService.search(main.Name)
         .then(function successCallBack(response){
         	main.array = response.data.items;
         	main.total = response.data.total_count;
 
         	
-        	console.log(response)
+        	console.log(main.array);
         }, function errorCallBack(response){
         	alert('Error!! check console');
         	console.log(response)
         });
        
 	}
+
+    //on keyup, start the countdown
+    $('#key').on('keyup', function () {
+         clearTimeout(main.typingTimer);
+         main.typingTimer = setTimeout(main.getRepository, 2000);
+     });
+
+    //on keydown, clear the countdown 
+     $('#key').on('keydown', function () {
+         clearTimeout(main.typingTimer);
+     });
+    
+
+
+
 	
 }]);
