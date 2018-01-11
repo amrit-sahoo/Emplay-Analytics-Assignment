1525e22ce4966634099367ec8d51e596d91dbc0e
Amrit Kumar Sahoo <31152533+amrit-sahoo@users.noreply.github.com>
2018-01-11 18:25:44 +0530

    Add files via upload

--- angular/services/service.js
+++ angular/services/service.js
@@ -1,13 +1,16 @@
-//service method
+// using service method
+
+//A custom service is defined for making a request to API. Generally we made custom service for making API request because in future when API changes it become easy to maintain.
+
 
 mainApp.service('gitService',function($http){
 
-	// this method first 
+	
 	var main =this;
 	var baseUrl = 'https://api.github.com/search/repositories';
 
 
-
+    //The search function is used in the controller.
 	this.search = function(query){
 
 		return $http({
@@ -18,7 +21,7 @@ mainApp.service('gitService',function($http){
 		})	
 
 
-	}// end search
+	}// end search function
 
 	
 	
